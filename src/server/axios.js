f2a9fa45710a593adbd9e73f7b6b8eb84834a418
Victor Gomez <vicgmezp@gmail.com>
2023-10-21 12:58:42 -0400

    Change timeout from 10000 to 1000

--- src/server/axios.js
+++ src/server/axios.js
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const server = axios.create({
   baseURL: `http://127.0.0.1:${process.env.EXPRESS_PORT}/`,
-  timeout: 10000,
+  timeout: 1000,
   headers: { "X-Custom-Header": "ShopSage API" },
 });
 
